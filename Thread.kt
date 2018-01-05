6fc5aea048ef0eabf1f2b032c78df8e8f3973618
Ayush Khanna <32393446+hhssuuyyaa@users.noreply.github.com>
2018-01-05 20:10:15 +0530

    Add files via upload

---
+++ Thread.kt
@@ -0,0 +1,44 @@
+
+fun main(args:Array<String>){
+
+    var t1= thread("thread1")
+    t1.start()
+
+    var t2= thread("thread2")
+    t2.start()
+
+    var t3= thread("thread3")
+    t3.start()
+    t3.join()                           //next line will not be printed until this thread is done
+    println("  thread is run")
+}
+
+
+
+
+class thread() :Thread(){
+    var ThreadName:String= ""
+
+    constructor(ThreadName:String):this(){
+        this.ThreadName=ThreadName
+        println(this.ThreadName+ " is started")
+    }
+
+    override  fun   run(){
+        //Write Thread
+
+
+        var count=0
+        while (count<10){
+            println(this.ThreadName+" Count:$count")
+            count++
+
+            try {
+                Thread.sleep(1000)
+            }catch (ex:Exception){
+                print(ex.message)
+            }
+        }
+    }
+
+}
